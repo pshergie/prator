6f816d8a99261d5874b66a725c0a07aca1e878cf
Maksim Minko <dethroll@gmail.com>
2024-05-23 17:48:53 +0200

    debugging

--- dist/index.js
+++ dist/index.js
@@ -37442,9 +37442,9 @@ async function run() {
     const token = core.getInput("token");
     const octokit = github.getOctokit(token);
     const context = github.context;
-    const pullNumber = parseInt(fs.readFileSync('pr_number.txt', "utf8"), 10);
-    const comments = JSON.parse(fs.readFileSync('pr_comments.json', "utf8"));
-    const diffFilesPaths = fs.readFileSync('pr_diff.txt', "utf8")?.split('\n').filter(Boolean);
+    const comments = JSON.parse(fs.readFileSync('auto_review_artifacts/pr_comments.json', "utf8"));
+    const pullNumber = parseInt(fs.readFileSync('auto_review_artifacts/pr_number.txt', "utf8"), 10);
+    const diffFilesPaths = fs.readFileSync('auto_review_artifacts/pr_diff.txt', "utf8")?.split('\n').filter(Boolean);
 
     checks.map(
       async ({ paths, message }) =>
