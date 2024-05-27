da83cbe8cfdcdb539cf0694eecb2573436a110f7
Maksim Minko <dethroll@gmail.com>
2024-05-27 22:39:32 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ async function run() {
     const token = core.getInput("token");
     const octokit = github.getOctokit(token);
     const context = github.context;
-    const comments = JSON.parse(fs.readFileSync(`${artifactsPath}pr_comments.json`, "utf8"));
-    const pullNumber = parseInt(fs.readFileSync(`${artifactsPath}pr_number.txt`, "utf8"), 10);
-    const diffFilesPaths = fs.readFileSync(`${artifactsPath}pr_diff.txt`, "utf8")?.split('\n').filter(Boolean);
+    const comments = JSON.parse(fs.readFileSync(`${artifactsPath}/pr_comments.json`, "utf8"));
+    const pullNumber = parseInt(fs.readFileSync(`${artifactsPath}/pr_number.txt`, "utf8"), 10);
+    const diffFilesPaths = fs.readFileSync(`${artifactsPath}/pr_diff.txt`, "utf8")?.split('\n').filter(Boolean);
 
     checks.map(
       async ({ paths, message }) =>
