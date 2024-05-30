30029c20c2b197d8eaaa0c67331926d55f8bb647
Maksim Minko <dethroll@gmail.com>
2024-05-30 21:05:25 +0200

    fix

--- src/utils/getCommentData.js
+++ src/utils/getCommentData.js
@@ -17,7 +17,7 @@ const getCommentData = () => {
     throw new Error('The comments data is empty or incorrect. ' + refMsg);
   }
 
-  if (!commentData.checksData) {
+  if (!commentData[1]) {
     console.log('Comment data: ', JSON.stringify(commentData, null, 4));
 
     throw new Error('Checks data is not correct. ' + refMsg);
